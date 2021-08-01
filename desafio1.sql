ae339da354d93b83b0df15001995c87e29478b48
gabzz <gabriel.menezes.dias@hotmail.com>
2021-08-01 15:12:01 -0300

    request 1

---
+++ desafio1.sql
@@ -0,0 +1,9 @@
+SELECT COUNTRY_NAME AS País,
+	CASE
+		WHEN REGION_ID = 1 THEN 'incluído'
+        WHEN REGION_ID = 2 THEN 'não incluído'
+        WHEN REGION_ID = 3 THEN 'não incluído'
+        WHEN REGION_ID = 4 THEN 'não incluído'
+	END AS 'Status inclusão'
+FROM hr.countries
+	ORDER BY COUNTRY_NAME ASC;
