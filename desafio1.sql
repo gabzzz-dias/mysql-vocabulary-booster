fab096bd7b74d1521c27473a3f08d89e8efb3821
gabzz <gabriel.menezes.dias@hotmail.com>
2021-08-01 15:16:49 -0300

    request 1 refactor

--- desafio1.sql
+++ desafio1.sql
@@ -1,9 +1,9 @@
 SELECT COUNTRY_NAME AS País,
-	CASE
-		WHEN REGION_ID = 1 THEN 'incluído'
-        WHEN REGION_ID = 2 THEN 'não incluído'
-        WHEN REGION_ID = 3 THEN 'não incluído'
-        WHEN REGION_ID = 4 THEN 'não incluído'
-	END AS 'Status inclusão'
+CASE
+WHEN REGION_ID = 1 THEN 'incluído'
+WHEN REGION_ID = 2 THEN 'não incluído'
+WHEN REGION_ID = 3 THEN 'não incluído'
+WHEN REGION_ID = 4 THEN 'não incluído'
+END AS 'Status inclusão'
 FROM hr.countries
-	ORDER BY COUNTRY_NAME ASC;
+ORDER BY COUNTRY_NAME ASC;
